d774ed352af7b4b3e7b5e65a186e499143f1d76a
Filip Johnsson <Filip.johnsson1989@gmail.com>
2022-01-17 09:52:08 +0100

    Bish-Bosh 2.0

--- bishbosh.js
+++ bishbosh.js
@@ -1,7 +1,7 @@
 "use strict";
 
 (function (w) {
-    
+
     const BishBoshEnum = {
         Bish: "Bish",
         Bosh: "Bosh"
@@ -43,20 +43,87 @@
             return resultArray;
         }
 
-        Print = ()=> this.#evaluateArray().join(', ');
-            
+        Print = () => this.#evaluateArray().join(', ');
+
     }
+
+    const setError = (input, message) => {
+        input.classList.remove('bg-success');
+        input.classList.remove('text-light');
+        input.classList.add('bg-danger');
+        input.classList.add('text-light');
+        
+        input.nextElementSibling.innerText=message;
+
+    }
+
+    const setSuccess = input => {
+        var hadError = input.classList.contains('bg-danger');
+        input.classList.remove('bg-danger');
+        input.classList.remove('text-light');
+
+        if (hadError) {
+            input.classList.add('bg-success');
+            input.classList.add('text-light');
+
+            input.nextElementSibling.innerText='';
+        }
+
+    }
+
+    const validateNumber = (input) => {
+        if (Number.parseInt(input.value) <= Number.parseInt(input.min)) {
+            setError(input, `Värdet får inte vara mindre än ${input.min}.`);
+            return false;
+        } else {
+            setSuccess(input)
+            return true;
+        }
+    }
+
+    const validate = (input) => {
+        switch (input.type) {
+            case 'number':
+                if (validateNumber(input))
+                    return true
+                else
+                    return false
+
+            default:
+                break;
+        }
+    }
+
+
+
+
 
     function run() {
-        const bishDivider = 3;
-        const boshDivider = 4;
-        const maxValue = 100;
+        const bishInput = document.querySelector('#bish');
+        const boshInput = document.querySelector('#bosh');
+        const maxInput = document.querySelector('#max');
 
-        const bishBosh = new BishBosh(bishDivider, boshDivider, maxValue)
+        const showBtn = document.querySelector('#show');
+        const resultH = document.querySelector('[for=result]');
+        const resultP = document.querySelector('#result');
 
-        console.log(bishBosh.Print());
+        showBtn.addEventListener('click', function () {
+
+            const bishDivider = bishInput.value;
+            const boshDivider = boshInput.value;
+            const maxValue = maxInput.value;
+
+            validate(bishInput);
+            validate(boshInput);
+            validate(maxInput);
+            const bishBosh = new BishBosh(bishDivider, boshDivider, maxValue)
+
+            resultH.classList.remove("d-none");
+            resultP.innerText = bishBosh.Print();
+        });
 
     }
+
 
     run();
 
